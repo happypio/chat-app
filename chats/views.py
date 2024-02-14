ef2b5abcba974a678af10285b8b4aad17bab1743
happypio <piterpiesiak@gmail.com>
2024-02-14 23:24:55 +0100

    Clean up chat view

--- chats/views.py
+++ chats/views.py
@@ -1,11 +1,8 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from django.shortcuts import redirect, render
 
 
 def chat(request):
     if request.user.is_authenticated:
         return render(request, "chats/chats.html")
     else:
-        return HttpResponseRedirect(reverse("custom_auth:login"))
+        return redirect("custom_auth:login")
