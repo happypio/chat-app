1e73c1a442d79732f982ccd5cbf31b3a0246864f
happypio <piterpiesiak@gmail.com>
2024-02-21 14:57:59 +0100

    Rename chats to index

--- chats/views.py
+++ chats/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import redirect, render
 
 def chat(request):
     if request.user.is_authenticated:
-        return render(request, "chats/chats.html")
+        return render(request, "chats/index.html")
     else:
         return redirect("custom_auth:login")
