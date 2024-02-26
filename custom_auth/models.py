5a35c6c9126d16a58195a9ec5e673800d50a8a28
happypio <piterpiesiak@gmail.com>
2024-02-26 18:44:34 +0100

    Customize user settings view

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -19,5 +19,5 @@ class CustomUser(AbstractUser):
     type = models.CharField(
         max_length=6,
         choices=TYPE_CHOICES,
-        help_text=("Type of account determines access to chat rooms"),
+        help_text=("Type of an account determines access to chat rooms"),
     )
